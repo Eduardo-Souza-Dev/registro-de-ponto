c00ea446c1ef481848455f36fa4a366591150c08
Eduardo Souza <denis.souzadeoliveira05@gmail.com>
2024-12-08 02:36:46 -0300

    Ajuste do build

--- backend/src/routes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-require("express-async-errors");
-const router = (0, express_1.Router)();
-exports.default = router;

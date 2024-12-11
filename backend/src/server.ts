a5ab748e1174d0dfb5a602ac4de202c2ff96aa5e
Eduardo Souza <denis.souzadeoliveira05@gmail.com>
2024-12-11 12:33:16 -0300

    Ajustando cors novamente

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,25 +2,28 @@ import express, {Request, Response, NextFunction} from 'express';
 import cors from 'cors';
 import router from './routes';
 
-
 const app = express();
+
+// Middleware para parsear JSON
 app.use(express.json());
-app.use(cors());
-app.use(router);
-const port = process.env.PORT || 4000;
 
-app.options('*', cors());
-
+// Configuração CORS
 app.use(cors({
-  origin: 'https://registro-de-ponto-six.vercel.app', // Origem permitida
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
+  origin: ['http://localhost:3000', 'https://registro-de-ponto-six.vercel.app'], // Adicione todas as origens permitidas
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos HTTP permitidos
   allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
 }));
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.error(err.stack);
-    res.status(500).send({ error: err.message });
-  });
+// Rotas
+app.use(router);
 
+// Middleware de tratamento de erros
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send({ error: err.message });
+});
+
+// Inicialização do servidor
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log("Servidor Online"));

f3f78b11981571022762a4471523f96e750e50d0
Eduardo Souza <denis.souzadeoliveira05@gmail.com>
2024-12-14 09:13:50 -0300

    Ajuste do front

--- frontend/src/app/ponto/page.tsx
+++ frontend/src/app/ponto/page.tsx
@@ -4,6 +4,7 @@ import styles from '../css/CodPage.module.css'
 import Link from 'next/link';
 import axios from 'axios';  // Certifique-se de instalar axios com `npm install axios` ou `yarn add axios`
 import { toast } from 'react-toastify';
+import { headers } from 'next/headers';
 
 const Ponto: React.FC = () => {
   const [codigoUsuario, setCodigoUsuario] = useState('');
@@ -38,6 +39,10 @@ const Ponto: React.FC = () => {
       console.log('ID do usuário:', id_usuario);
   
       const response = await axios.post('https://registro-de-ponto.onrender.com/entry/register', {
+        withCredentials: true,
+        headers:{
+          'Content-Type': 'application/json',
+        },
         id_usuario,
       });
   
